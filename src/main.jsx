5e5c01e3f9ebb98d33b4873f10dfe6ce5abb52df
roaayala <roaayala.ahm@gmail.com>
2026-07-20 20:12:16 +0800

    setup initial routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { createBrowserRouter, RouterProvider } from "react-router";
+
 import "./assets/global.css";
-import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import Shop from "./pages/Shop.jsx";
+import Cart from "./pages/Cart.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "shop", element: <Shop /> },
+  { path: "cart", element: <Cart /> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );

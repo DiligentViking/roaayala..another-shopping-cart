3d957108fbaba08d35cd4f89819f75b8c54895f8
roaayala <roaayala.ahm@gmail.com>
2026-07-20 20:41:56 +0800

    Container

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,20 +1,23 @@
 import { Link, Outlet } from "react-router";
+import Container from "../components/Container/Container";
 
 function RootLayout() {
   return (
     <div>
       <nav>
-        <ul>
-          <li>
-            <Link to={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link to={"shop"}>Shop</Link>
-          </li>
-          <li>
-            <Link to={"cart"}>Cart</Link>
-          </li>
-        </ul>
+        <Container>
+          <ul>
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"shop"}>Shop</Link>
+            </li>
+            <li>
+              <Link to={"cart"}>Cart</Link>
+            </li>
+          </ul>
+        </Container>
       </nav>
 
       <main>

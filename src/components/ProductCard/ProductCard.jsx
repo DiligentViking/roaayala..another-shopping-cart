2022acafc6b30794ddadc0ea8c16f0353c91afd0
roaayala <roaayala.ahm@gmail.com>
2026-07-22 12:10:57 +0800

    ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,13 +4,16 @@ function ProductCard({ product }) {
   return (
     <div className={styles.card}>
       <div className={styles.cardCover}>
-        <img src={product.image} alt={product.title} />
+        <img
+          className={styles.cardCoverImage}
+          src={product.image}
+          alt={product.title}
+        />
       </div>
 
       <div className={styles.cardInfo}>
         <h3 className={styles.cardInfoTitle}>{product.title}</h3>
-        <p className={styles.cardInfoDescription}>{product.description}</p>
-        <span className={styles.carfInfoPrice}>{product.price}</span>
+        <span className={styles.cartInfoPrice}>$ {product.price}</span>
       </div>
 
       <div className={styles.cardActions}>

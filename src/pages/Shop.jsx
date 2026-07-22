001ae5db00e0a245bff3e6a41c4cf1cf47cc0cec
roaayala <roaayala.ahm@gmail.com>
2026-07-22 15:53:47 +0800

    Shop: filter by category

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container, ProductCard, ProductsContainer } from "../components";
 import useProduct from "../hooks/useProduct";
 
@@ -5,23 +6,40 @@ import styles from "./Shop.module.css";
 
 function Shop() {
   const { products, productCategories, isLoading } = useProduct();
-  console.log(productCategories);
+  const [activeCategory, setActiveCategory] = useState(null);
+
+  const displayedProducts = activeCategory
+    ? products.filter((product) => product.category === activeCategory)
+    : products;
 
   return (
     <div className={styles.shop}>
       <Container>
         <div>
-          <ul>
+          <ul
+            onClick={(e) => {
+              const target = e.target.closest("li");
+
+              if (!target) return;
+
+              const targetName = target.getAttribute("data-category");
+              setActiveCategory(targetName === "null" ? null : targetName);
+            }}
+          >
+            <li data-category="null">All</li>
             {productCategories.map((category) => (
-              <li key={category.id}>{category.name}</li>
+              <li key={category.id} data-category={category.name}>
+                {category.name}
+              </li>
             ))}
           </ul>
         </div>
+
         <ProductsContainer>
           {isLoading ? (
             <>Loading...</>
           ) : (
-            products.map((product) => (
+            displayedProducts.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))
           )}

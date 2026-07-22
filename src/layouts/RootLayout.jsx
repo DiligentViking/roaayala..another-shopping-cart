6103347bdbfc967e4039b9eb6bf9cae98ceb0fd8
roaayala <roaayala.ahm@gmail.com>
2026-07-22 20:47:50 +0800

    RootLayout: style

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router";
 import { Navbar } from "../components";
+import styles from "./RootLayout.module.css";
 
 function RootLayout() {
   return (
-    <div>
+    <div className={styles.rootLayout}>
       <Navbar />
-      <main>
+      <main className={styles.main}>
         <Outlet />
       </main>
     </div>

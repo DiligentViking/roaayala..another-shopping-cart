13fb1008d7fe0ad873dbcc36ab1ccf3b56d3e675
roaayala <roaayala.ahm@gmail.com>
2026-07-27 14:58:50 +0800

    ErrorPage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router";
+import { Container } from "../../components";
+
 function ErrorPage() {
-  return <div>404</div>;
+  return (
+    <Container>
+      <h2>Page Not Found</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium,
+        perferendis ullam necessitatibus soluta enim illo voluptas aliquid sunt
+        iusto dicta.
+      </p>
+
+      <Link to="/">Back to Home</Link>
+    </Container>
+  );
 }
 
 export default ErrorPage;

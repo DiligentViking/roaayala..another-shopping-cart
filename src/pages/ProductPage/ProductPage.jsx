34a9037f3f8a09e0e21f1bafc49d4f559117d432
DiligentViking <acore-n@proton.me>
2026-08-05 14:58:51 -0500

    Change strategy of data passing
    The previous strategy is more optimized, but does not work in situations where the user visits the product page in any way other than clicking on the item in the shop.

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,8 +1,14 @@
-import { useLocation } from "react-router";
+import { useParams } from "react-router";
+import useProduct from "../../hooks/useProduct/useProduct";
 
 function ProductPage() {
-  const location = useLocation();
-  const product = location.state?.product;
+  const urlParams = useParams();
+  const { products } = useProduct();
+
+  const productId = +urlParams.productId;
+  const product = products.find((item) => item.id === productId);
+
+  if (!product) return <></>;
 
   return <div>{product.title}</div>;
 }

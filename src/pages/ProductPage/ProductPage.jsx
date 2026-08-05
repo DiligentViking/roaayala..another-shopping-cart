d659b8b880d31cf6d86c28668cb13ec83767fcca
DiligentViking <acore-n@proton.me>
2026-08-05 15:46:37 -0500

    Add content

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
 import { useParams } from "react-router";
+
 import useProduct from "../../hooks/useProduct/useProduct";
+import { CartContext } from "../../contexts/context";
+import { ItemCounter } from "../../components";
 
 function ProductPage() {
   const urlParams = useParams();
@@ -8,9 +12,30 @@ function ProductPage() {
   const productId = +urlParams.productId;
   const product = products.find((item) => item.id === productId);
 
+  const { cart, addToCart, removeFromCart } = useContext(CartContext);
+
+  const cartItem = cart.find((item) => item.id === productId);
+  const itemQuantity = cartItem?.quantity;
+
   if (!product) return <></>;
 
-  return <div>{product.title}</div>;
+  return (
+    <div>
+      <h1>{product.title}</h1>
+      <img src={product.image} alt="" />
+      <p>{product.description}</p>
+      <p>
+        <b>{product.category}</b>
+      </p>
+      <data value={product.price}>${product.price}</data>
+      <ItemCounter
+        product={product}
+        itemQuantity={itemQuantity}
+        addToCart={addToCart}
+        removeFromCart={removeFromCart}
+      />
+    </div>
+  );
 }
 
 export default ProductPage;
